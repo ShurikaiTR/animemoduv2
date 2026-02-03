a876273a9bb9ecb43bcc914992ac29995ece4f2e
ShurikaiTR <gknguler1995@gmail.com>
2026-02-03 13:32:03 +0300

    refactor: rename profile component and simplify routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Livewire\Pages\Home;
-use App\Livewire\Profile\Show;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::livewire('/animeler', 'anime.hub')->name('anime.hub');
 Route::livewire('/filmler', 'movie.index')->name('movie.index');
 Route::livewire('/', 'pages.home')->name('home');
-Route::livewire('/profil/{username?}', 'profile.show')->name('profile.show');
+Route::livewire('/profil/{username?}', 'profile')->name('profile');
 Route::livewire('/anime/{slug}', 'anime.show')->name('anime.show');
 Route::livewire('/izle/{anime:slug}/{segment1}/{segment2?}', 'anime.watch')->name('anime.watch');
 Route::livewire('/kesfet', 'anime.discover')->name('anime.discover');
